8c9cc81d23e71d4ea8592838ab61a2f6714beb1b
kshravi86 <kshravi86@gmail.com>
2018-01-16 22:07:13 +0530

    hh

--- js/core/app.js
+++ js/core/app.js
@@ -17,20 +17,20 @@ App.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'templates/Keyframes.html'
         }).when("/keyframes69",{templateUrl:'templates/keyframes69.html'}).when("/keyframes3", {
             templateUrl: 'templates/keyframes3.html',
-            
+            controller: 'Transitions'
         }).when("/keyframes15", {
             templateUrl: 'templates/keyframes15.html'
         }).when("/keyframes87",{templateUrl:'templates/keyframes87.html'}).when("/keyframes57",{templateUrl:'templates/keyframes57.html'}).when("/keyframes18", {
             templateUrl: 'templates/keyframes18.html'
         }).when("/keyframes24", {
             templateUrl: 'templates/keyframes24.html'
-        }).when("/keyframes102",{templateUrl:'templates/keyframes102.html'}).when("/keyframes96",{templateUrl:'templates/keyframes96.html'}).when("/keyframes93",{templateUrl:'templates/keyframes93.html'}).when("/keyframes90",{templateUrl:'templates/keyframes90.html'}).when("/keyframes60",{templateUrl:'templates/keyframes60.html'}).when("/keyframes51",{templateUrl:'templates/keyframes51.html'})
+        }).when("/keyframes181",{templateUrl:'templates/keyframes181.html'}).when("/keyframes178",{templateUrl:'templates/keyframes178.html'}).when("/keyframes175",{templateUrl:'templates/keyframes175.html'}).when("/keyframes172",{templateUrl:'templates/keyframes172.html'}).when("/keyframes169",{templateUrl:'templates/keyframes169.html'}).when("/keyframes166",{templateUrl:'templates/keyframes166.html'}).when("/keyframes163",{templateUrl:'templates/keyframes163.html'}).when("/keyframes160",{templateUrl:'templates/keyframes160.html'}).when("/keyframes157",{templateUrl:'templates/keyframes157.html'}).when("/keyframes154",{templateUrl:'templates/keyframes154.html'}).when("/keyframes151",{templateUrl:'templates/keyframes151.html'}).when("/keyframes148",{templateUrl:'templates/keyframes148.html'}).when("/keyframes145",{templateUrl:'templates/keyframes145.html'}).when("/keyframes142",{templateUrl:'templates/keyframes142.html'}).when("/keyframes139",{templateUrl:'templates/keyframes139.html'}).when("/keyframes136",{templateUrl:'templates/keyframes136.html'}).when("/keyframes133",{templateUrl:'templates/keyframes133.html'}).when("/keyframes133",{templateUrl:'templates/keyframes133.html'}).when("/keyframes130",{templateUrl:'templates/keyframes130.html'}).when("/keyframes127",{templateUrl:'templates/keyframes127.html'}).when("/keyframes124",{templateUrl:"templates/keyframes124.html"}).when("/keyframes121",{templateUrl:'templates/keyframes121.html'}).when("/keyframes118",{templateUrl:"templates/keyframes118.html"}).when("/keyframes102",{templateUrl:'templates/keyframes102.html'}).when("/keyframes96",{templateUrl:'templates/keyframes96.html'}).when("/keyframes93",{templateUrl:'templates/keyframes93.html'}).when("/keyframes90",{templateUrl:'templates/keyframes90.html'}).when("/keyframes60",{templateUrl:'templates/keyframes60.html'}).when("/keyframes51",{templateUrl:'templates/keyframes51.html'})
 		.when("/keyframes27", {
             templateUrl: 'templates/keyframes27.html'
-        }).when("/keyframes54",{templateUrl:'templates/keyframes54.html'}).when("/keyframes30", {
+        }).when("/keyframes115",{templateUrl:'templates/keyframes115.html'}).when("/keyframes105",{templateUrl:'templates/keyframes105.html'}).when("/keyframes54",{templateUrl:'templates/keyframes54.html'}).when("/keyframes30", {
             templateUrl: 'templates/keyframes30.html'
         }).when("/keyframes45",{templateUrl:'templates/keyframes45.html'
-		}).when("/keyframes102",{templateUrl:'templates/keyframes102.html'}).when("/keyframes99",{ templateUrl:'templates/keyframes99.html' }).when("/keyframes48",{templateUrl:'templates/keyframes48.html'}).when("/keyframes39",{templateUrl:'templates/keyframes39.html'}).
+		}).when("/keyframes112",{templateUrl:'templates/keyframes112.html'}).when("/keyframes108",{templateUrl:'templates/keyframes108.html'}).when("/keyframes48",{templateUrl:'templates/keyframes48.html'}).when("/keyframes39",{templateUrl:'templates/keyframes39.html'}).
 		otherwise({
             redirectTo: '/'
         });
